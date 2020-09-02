df4e2ba80510db05219f53a0d4f414c333f9b6be
Norieee <aok76283@gmail.com>
2020-09-02 15:48:09 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :messages
+  has_many :pro_users
+
+  japanese_letters = /\A[ぁ-んァ-ン一-龥]+\z/
+  with_options presence: true do
+    validates :last_name, format: { with: japanese_letters, message: 'is not full-width characters' }
+    validates :first_name, format: { with: japanese_letters, message: 'is not full-width characters' }
+    validates :email, uniqueness: { case_sensitive: true },
+               format: { with: /\w+([-+.]\w+)*@\w+([-.]\w+)*/ }
+    validates :password, length: {minimum: 6 },
+               format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+\z/, message: 'includes both half-width letters and half-width numbers' }
+    validates :company, :password_confirmation
+  end
 end

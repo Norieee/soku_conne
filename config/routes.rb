c2d206f8bb712618cadc3762a7b99eef13167f2d
Norieee <aok76283@gmail.com>
2020-09-04 21:33:18 +0900

    プロジェクト新規登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'messages#index'
   resources :projects, only: [:new, :create]
 end

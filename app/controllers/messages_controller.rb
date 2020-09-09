ab1f5e1b2ff9402f035c0831388664e6f447b402
Norieee <aok76283@gmail.com>
2020-09-10 07:13:12 +0900

    メッセージスペースへの画面遷移

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
   def index
+    @project = Project.find(params[:project_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  root to: 'messages#index'
-  resources :projects, only: [:new, :create]
+  root to: 'projects#index'
+  resources :projects, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
+  
   resources :qas do
     collection { post :import }
     collection { get :export }
